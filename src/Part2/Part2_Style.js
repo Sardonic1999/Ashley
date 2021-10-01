0711899cf2657914312b52d15de8538ec91736f2
Sardonic1999 <sardorjurayev578@gmail.com>
2021-10-01 23:47:16 +0500

    Part2

---
+++ src/Part2/Part2_Style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 1520px;
+  height: 800px;
+  background: #e7f066;
+  border-radius: 0px;
+  box-sizing: border-box;
+  h1 {
+    font-family: Arial, Helvetica, sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 25px;
+    line-height: 32px;
+    letter-spacing: 0.5px;
+
+    color: #262626;
+  }
+`;
--- src/Root.jsx
+++ src/Root.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Part1 from "./Part1/Part1";
+import Part2 from "./Part2/Part2";
 
 const Root = () => {
   return (
     <div>
       <Part1 />
+      <Part2 />
     </div>
   );
 };
