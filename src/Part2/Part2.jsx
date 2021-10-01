9e699dedbb5a4045dec415cfe08b9c0fecf4e8ff
Sardonic1999 <sardorjurayev578@gmail.com>
2021-10-02 00:04:52 +0500

    Part2 edit

--- src/Part2/Part2.jsx
+++ src/Part2/Part2.jsx
@@ -1,8 +1,24 @@
 import React from "react";
-import { Wrapper } from "./Part2_Style";
+import { Container, Img2, Wrapper, Text } from "./Part2_Style";
+import img2 from "../assets/img2.png";
 
 const Part2 = () => {
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <Container>
+        <Text>
+          <h2>HAND POURED CANDLES</h2>
+          <p>
+            The flicker of the wick and the <br /> wafting of scent…Waxed
+            Perfume is <br />
+            the simplest daily self-indulgence.
+          </p>
+          <h4>browse our full collection</h4>
+        </Text>
+        <Img2 src={img2} />
+      </Container>
+    </Wrapper>
+  );
 };
 
 export default Part2;
--- src/Part2/Part2_Style.js
+++ src/Part2/Part2_Style.js
@@ -3,17 +3,64 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   width: 1520px;
   height: 800px;
-  background: #e7f066;
+  background: #9c8554;
   border-radius: 0px;
   box-sizing: border-box;
-  h1 {
+`;
+
+export const Container = styled.div`
+  display: flex;
+  box-sizing: border-box;
+`;
+
+export const Img2 = styled.img`
+  width: 1065px;
+  height: 688px;
+  margin-left: auto;
+`;
+
+export const Text = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 450px;
+  height: fit-content;
+  padding: 40px;
+  box-sizing: border-box;
+  h2 {
     font-family: Arial, Helvetica, sans-serif;
     font-style: normal;
     font-weight: normal;
-    font-size: 25px;
-    line-height: 32px;
+    font-size: 15px;
+    line-height: 19px;
     letter-spacing: 0.5px;
-
-    color: #262626;
+    margin: 0px;
+    color: #b2b2b2;
+  }
+  p {
+    font-family: Arial, Helvetica, sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 30px;
+    letter-spacing: 0.5px;
+    margin: 0px;
+    padding-top: 580px;
+    color: #b2b2b2;
+  }
+  h4 {
+    font-family: Arial, Helvetica, sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 15px;
+    line-height: 19px;
+    letter-spacing: 1.2px;
+    text-transform: uppercase;
+    border-bottom: 1px solid transparent;
+    color: #b2b2b2;
+    width: 280px;
+    margin: 0px;
+    :hover {
+      border-bottom: 1px solid white;
+    }
   }
 `;
